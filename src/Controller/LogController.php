ecc63c54a1c2e82ff9f07e25924dae48f2315675
Stephane Delbeke <stephane.delbeke@enabel.be>
2019-02-25 22:30:43 +0100

    wip log table

--- src/Controller/LogController.php
+++ src/Controller/LogController.php
@@ -9,19 +9,54 @@
 namespace App\Controller;
 
 
+use App\Entity\Job;
+use App\Entity\Log;
+use Doctrine\DBAL\Types\DateTimeType;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class LogController {
+class LogController extends AbstractController {
 
   /**
    * @Route("/")
    */
 
-  public function homepage()
+  public function homepage(EntityManagerInterface $em)
   {
-      return $this->render('log/homepage.html.twig');
+    $job_repository = $em->getRepository(Job::class);
+    $jobs = $job_repository->findAll();
+    $repository = $em->getRepository(Log::class);
+    $job_logs = [];
+    $job_logs_date = [];
+    foreach ($jobs as $job) {
+      $j_logs = $repository->findLatestByJob();
+      foreach($j_logs as $j_log){
+//var_dump($j_log->getStart());
+
+       // $l_date = date("d-m-Y", strtotime($j_log->getStart()));
+        $l_date = $j_log->getStart()->format('Y-m-d');
+        $job_logs_date[$l_date] = $j_log;
+
+      }
+
+      $job_logs[$job->getName()] = $job_logs_date;
+    }
+
+
+    $m= date("m");
+
+    $de= date("d");
+
+    $y= date("Y");
+    for($i=7; $i>=0; $i--){
+      $log_dates[$i] = date('Y-m-d',mktime(0,0,0,$m,($de-$i),$y));
+    }
+
+      return $this->render('log/homepage.html.twig',['job_logs' => $job_logs, 'log_dates' => $log_dates]);
+
   }
 
 }
---
+++ src/Entity/Log.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LogRepository")
+ */
+class Log
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $start;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $end;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $interface_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStart(): ?\DateTimeInterface
+    {
+        return $this->start;
+    }
+
+    public function setStart(?\DateTimeInterface $start): self
+    {
+        $this->start = $start;
+
+        return $this;
+    }
+
+    public function getEnd(): ?\DateTimeInterface
+    {
+        return $this->end;
+    }
+
+    public function setEnd(?\DateTimeInterface $end): self
+    {
+        $this->end = $end;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getInterfaceId(): ?int
+    {
+        return $this->interface_id;
+    }
+
+    public function setInterfaceId(int $interface_id): self
+    {
+        $this->interface_id = $interface_id;
+
+        return $this;
+    }
+}
