c3a9b3aa6c904093117ee86b0a3be26de4ff85f8
Damien LAGAE <damien.lagae@enabel.be>
2019-02-28 16:47:51 +0100

    :bug: Fix migrations

--- src/Migrations/Version20190228154548.php
+++ src/Migrations/Version20190228154548.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare (strict_types = 1);
 
 namespace DoctrineMigrations;
 
@@ -10,30 +10,30 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190226094909 extends AbstractMigration
+final class Version20190228154548 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE job ADD frequency VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE TABLE log (id INT AUTO_INCREMENT NOT NULL, job_id INT NOT NULL, start DATETIME DEFAULT NULL, end DATETIME DEFAULT NULL, status VARCHAR(255) DEFAULT NULL, message LONGTEXT DEFAULT NULL, INDEX IDX_8F3F68C5BE04EA9 (job_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE job (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, frequency VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE log ADD CONSTRAINT FK_8F3F68C5BE04EA9 FOREIGN KEY (job_id) REFERENCES job (id)');
-        $this->addSql('CREATE INDEX IDX_8F3F68C5BE04EA9 ON log (job_id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE job DROP frequency');
         $this->addSql('ALTER TABLE log DROP FOREIGN KEY FK_8F3F68C5BE04EA9');
-        $this->addSql('DROP INDEX IDX_8F3F68C5BE04EA9 ON log');
+        $this->addSql('DROP TABLE log');
+        $this->addSql('DROP TABLE job');
     }
 }
